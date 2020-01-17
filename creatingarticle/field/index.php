f504ed610b6bf6f6ad529a74980a408b0ad2dfb3
DenisProgrammer <30808396+scisoftdev@users.noreply.github.com>
2020-01-17 18:35:59 +0400

    Create index.php

---
+++ creatingarticle/field/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="" method="post">
+		<p>Ваше имя: <input type="text" name="name"/></p>
+		<p>Ваш возраст: <input type="text" name="age"/></p>
+		<p><input type="submit"></p>
+	</form>
+
+	Привет! <?php echo htmlspecialchars($_POST['name']); ?>
+	Вам <?php echo htmlentities($_POST['age']); ?> лет.
+
+	<?php 
+		echo "Данные из адресной строки: {$_SERVER['QUERY_STRING']}";
+	 ?>
+
+	<form action="<?php echo htmlentities($_SERVER['PHP_SELF'])?>" method="post">
+		<fieldset>
+			<legend>Выберите животное</legend>
+			<label for="dog">
+			<input type="checkbox" id="dog" name="animal[]" value=" собака">Собака
+			</label>
+			<label for="cat">
+			<input type="checkbox" id="cat" name="animal[]" value=" кошка">Кошка
+			</label>
+			<label for="fox">
+			<input type="checkbox" id="fox" name="animal[]" value=" лиса">Лиса
+			</label>
+			<label for="tiger">
+			<input type="checkbox" id="tiger" name="animal[]" value=" тигр">Тигр
+			</label>
+		</fieldset>
+		<input type="submit" value="отправить">
+	</form>
+
+	 <?php 
+	 	$animal = isset($_POST['animal']) ? $_POST['animal']: '';
+	 	if (!empty($animal)){
+	 		echo '<br><br> Выбраны:';
+	 		foreach ($animal as $a) {
+	 			echo "<span style=\"color:green\">". htmlentities($a)."</span>";
+	 		}
+	 	}
+	  ?>
+</body>
+</html>
